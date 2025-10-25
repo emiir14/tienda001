4d94370abdbd17c6b9daacbddd2120793e42e860
Emir Ale <emirderespaldo@gmail.com>
2025-10-25 01:06:53 +0000

    Use this address there: La Rioja 416, Catamarca, Argentina (_for element

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -4,7 +4,7 @@
 export default function TopBar() {
   return (
     <div className="bg-secondary/50 text-secondary-foreground text-center p-2 text-sm font-medium">
-      <p>INDEPENDENCIA 507 - CORDOBA</p>
+      <p>La Rioja 416, Catamarca, Argentina</p>
     </div>
   );
 }
